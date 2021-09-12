0fdbcfd3fb3377902822369461d0440442e81773
Agustin Cundari <acundari95@gmail.com>
2021-09-12 19:26:02 -0300

    Nueva versión del proyecto
    Se actualiza la versión del proyecto con todo lo desarrollado hasta la fecha (y todo lo perdido en la reestructuracion del proyecto)

--- Assets/backPathChecker.cs
+++ Assets/backPathChecker.cs
@@ -14,7 +14,7 @@ public class backPathChecker : MonoBehaviour
         Ray ray = new Ray(transform.position, -transform.up);
         RaycastHit rayInfo;
 
-        if (Physics.Raycast(ray, out rayInfo, 100) && (rayInfo.collider.CompareTag("walkable") || rayInfo.collider.CompareTag("jumpeable")))
+        if (Physics.Raycast(ray, out rayInfo, 100) && (rayInfo.collider.CompareTag("walkable") || rayInfo.collider.CompareTag("jumpeable")|| rayInfo.collider.CompareTag("Ladder")))
         {
             playerControl.canMoveBackward = true;
             Debug.DrawLine(ray.origin, rayInfo.point, Color.green);
